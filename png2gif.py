540c0c4a4d0162cee8a2df9d585e34ce5d9ccde6
Jiaowen Yang <yjwalbers@gmail.com>
2017-11-28 04:14:26 +0000

    Add stay top image

--- png2gif.py
+++ png2gif.py
@@ -1,11 +1,11 @@
 import imageio
 
-left = 45
-right = 120
+left = 3800
+right = 4000
 filenames = []
 images = []
 for i in range(left, right+1):
     filenames.append('img_' + str(i) + '.png')
 for f in filenames:
     images.append(imageio.imread(f))
-imageio.mimsave('img_v0_good_2.gif', images)
+imageio.mimsave('img_stay_top.gif', images)
